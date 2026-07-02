e290e499cdecab28cdc54cc15d6498e7ba97d3f2
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-07-02 09:21:32 +0530

    Update localStorage.js

--- localStorage.js
+++ localStorage.js
@@ -1,14 +1,21 @@
-let key = prompt("Enter key you want to set")
-let value = prompt("Enter value you want to set")
+let key = prompt("Enter key you want to set");
+let value = prompt("Enter value you want to set");
 
-localStorage.setItem(key, value)
-
-console.log(`The value at ${key} is ${localStorage.getItem(key)}`)
-
-if (key == "red" || key == "blue") {
-    localStorage.removeItem(key)
-}
-
-if (key == 0) {
-    localStorage.clear()
+if (key) {
+    localStorage.setItem(key, value);
+    console.log(`The value at ${key} is ${localStorage.getItem(key)}`);
+    
+   
+    if (key === "red" || key === "blue") {
+        localStorage.removeItem(key);
+        console.log(`${key} was removed immediately`);
+    }
+    
+    
+    if (key === "0") {
+        localStorage.clear();
+        console.log("localStorage cleared");
+    }
+} else {
+    console.log("No key entered. Nothing stored.");
 }
